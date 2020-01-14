1ec08f24ae9710947143f2bfbdf2fe2dbcaafc06
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2020-01-14 13:20:33 -0500

    project setup

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code away!
+const express = require('express');
+const userRoutes = require('./users/userRouter');
+const postRoutes = require('./posts/postRouter');
+
+const server = express();
+server.use(express.json());
+
+server.use('/api/users', userRoutes);
+server.use('/api/posts', postRoutes);
+
+const port = 8000;
+server.listen(port, () => console.log(`Listening on port ${port}`));
